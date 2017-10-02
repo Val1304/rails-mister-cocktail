ca66bb7b8720f83100f5631276e5ce4242bd67bf
Valentin Le Blan <valentinleblan@gmail.com>
2017-10-02 15:36:43 +0200

    Controllers OK

--- Gemfile
+++ Gemfile
@@ -34,7 +34,10 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
-gem 'jquery-rails' # Add this line if you use Rails 5.1
+gem 'jquery-rails'
+
+gem 'faker'
+ # Add this line if you use Rails 5.1
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 20 fake Ingredients...'
+20.times do
+  ingredient = Ingredient.new(
+    name: Faker::Food.ingredient,
+    )
+  ingredient.save!
+end
+
+puts 'Finished'
